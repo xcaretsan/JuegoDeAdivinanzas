5a1ee0a7e5029acda62bbc24731d12bf785ab39a
xcaretsan <143143348+xcaretsan@users.noreply.github.com>
2025-03-27 23:00:02 -0600

    Update juegoAdivinanza.java
    **Resumen breve:**  
    
    En el bucle "while(intentos < maxIntentos)" permite hasta 10 intentos. Se solicita un número, se valida que sea un entero y esté en el rango 1-100. Si la entrada es válida, se incrementa el contador de intentos. Luego, se compara con el número aleatorio: si es correcto, el juego termina; si es menor o mayor, se indica al usuario. Si se agotan los intentos sin adivinar, se muestra el número correcto.

--- juegoAdivinanza.java
+++ juegoAdivinanza.java
@@ -1,19 +1,43 @@
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class JuegoAdivinanza {
     public static void main(String[] args) {
+        Random rand = new Random();
+        int numeroAleatorio = rand.nextInt(100) + 1; 
+        Scanner scanner = new Scanner(System.in);
         int intentos = 0;
         int maxIntentos = 10;
+        int numeroUsuario = 0;
         boolean adivinado = false;
-        
+        boolean entradaValida = false;
+
         System.out.println("¡Bienvenido al juego de adivinanza!");
         System.out.println("Debes adivinar un número entre 1 y 100.");
         System.out.println("Tienes 10 intentos. ¡Buena suerte!");
 
         while(intentos < maxIntentos){
-            if (numeroUsuario == numeroAleatorio) {
+
+            System.out.println("Intento "+ (intentos+1) +" de "+ maxIntentos +". Introduce un numero: ");
+            
+            if (scanner.hasNextInt()) {
+                numeroUsuario = scanner.nextInt();
+                entradaValida = true;
+            } 
+            else {
+                System.out.println("Error: Entrada no valida. Por favor, ingresa un número entre 1 y 100.");
+                scanner.next(); 
+            }
+
+                 if (numeroUsuario < 1 || numeroUsuario > 100) {
+                    System.out.println("Número fuera de rango. Introduce un número entre 1 y 100.");
+                    continue;
+                }
+
+                if(entradaValida==true)
+                 intentos++;
+
+                if (numeroUsuario == numeroAleatorio) {
                     System.out.println("¡Felicidades! Has adivinado el número en " + intentos + " intentos.");
                     adivinado = true;
                     break;
@@ -24,11 +48,14 @@ public class JuegoAdivinanza {
                 
                 else 
                     System.out.println("El número es menor. Intenta de nuevo.");
+                
+
         }
-        if (!adivinado) 
-        System.out.println("Lo siento, has agotado tus intentos."");
 
-        scanner.close();
+            if (!adivinado) 
+            System.out.println("Lo siento, has agotado tus intentos. El número era " + numeroAleatorio);
 
-    }
+            scanner.close();
+
+ }
 }
